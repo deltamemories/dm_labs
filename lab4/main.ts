1f52df0fc17757a94c5665fb968de75714649983
deltamemories <maxenot123@gmail.com>
2026-02-27 22:50:51 +0200

    lab4: complete 1. of task & fixes

--- lab4/main.ts
+++ lab4/main.ts
@@ -8,16 +8,17 @@ function readText(filepath: string) {
     try {
         return fs.readFileSync(filepath, 'utf8')
     } catch (err) {
-        console.log(err)
+        throw err
     }
 }
+
 
 function formatText(text: string) {
     text = text.trim()
     text = text.toLowerCase()
-    text = text.replace('\n', '')
-    text = text.replace('\t', '')
-    text = text.replace(' ', '')
+    text = text.replaceAll('\n', '')
+    text = text.replaceAll('\t', '')
+    text = text.replaceAll(' ', '')
 
     let resultText = ''
     for (const char of text) {
@@ -26,16 +27,16 @@ function formatText(text: string) {
         }
     }
     return resultText
-
 }
 
 
 interface StaticAnalyzeResult {
     charCounts: Map<string, number>
-    charFrequency: Map<string, number>
+    charFrequencies: Map<string, number>
     pairsCounts: Map<string, number>
-    pairsFrequency: Map<string, number>
+    pairsFrequencies: Map<string, number>
 }
+
 
 function staticAnalyzeText(text: string): StaticAnalyzeResult {
     const textLen = text.length
@@ -43,34 +44,33 @@ function staticAnalyzeText(text: string): StaticAnalyzeResult {
     let charCounts = new Map<string, number>()
 
     for (const char of text) {
-        charCounts.set(char, charCounts.get(char) ? charCounts.get(char) + 1 : 1)
+        charCounts.set(char, charCounts.get(char) ? charCounts.get(char)! + 1 : 1)
     }
 
     let charFreq = new Map<string, number>()
-    for (const char of Object.keys(charCounts)) {
-        charFreq.set(char, charCounts.get(char) / textLen)
+    for (const char of charCounts.keys()) {
+        charFreq.set(char, charCounts.get(char)! / textLen)
     }
 
     let pairCounts = new Map<string, number>()
 
     for (let i = 0; i<textLen-1; i++) {
         const pair = text.slice(i, i + 2)
-        pairCounts.set(pair, pairCounts.get(pair) ? pairCounts.get(pair) + 1 : 1)
+        pairCounts.set(pair, pairCounts.get(pair) ? pairCounts.get(pair)! + 1 : 1)
     }
 
     let pairFreq = new Map<string, number>()
 
-    for (const pair of Object.keys(pairCounts)) {
-        pairFreq.set(pair, pairCounts.get(pair) / textLen)
+    for (const pair of pairCounts.keys()) {
+        pairFreq.set(pair, pairCounts.get(pair)! / textLen)
     }
 
     return {
-        charCounts,
-        charFreq,
-        pairCounts,
-        pairFreq
+        charCounts: charCounts,
+        charFrequencies: charFreq,
+        pairsCounts: pairCounts,
+        pairsFrequencies: pairFreq
     }
-
 }
 
 
